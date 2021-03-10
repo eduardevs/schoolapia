1fccca351766a704a70433bd0f73f9b1334876cc
Eduardo <jeseduardopi@gmail.com>
2021-03-10 09:37:55 +0100

    table Actualite

---
+++ src/Entity/Actualite.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActualiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity(repositoryClass=ActualiteRepository::class)
+ * @Vich\Uploadable
+ */
+class Actualite
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $soustitre;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $texte;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $image;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="image_upload", fileNameProperty="image")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedat;
+
+
+    public function __construct()
+    {
+        $this->updateAt = new \DateTimeImmutable();
+    }
+    public function __toString()
+    {
+        if (is_null($this->titre)) {
+            return "";
+        }
+        return $this->titre;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getSoustitre(): ?string
+    {
+        return $this->soustitre;
+    }
+
+    public function setSoustitre(string $soustitre): self
+    {
+        $this->soustitre = $soustitre;
+
+        return $this;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(?string $texte): self
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updateAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function getUpdatedat(): ?\DateTimeInterface
+    {
+        return $this->updatedat;
+    }
+
+    public function setUpdatedat(\DateTimeInterface $updatedat): self
+    {
+        $this->updatedat = $updatedat;
+
+        return $this;
+    }
+}
