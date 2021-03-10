9e3eeb5ba45b89d600832b469ba0b28b7d735be3
Eduardo <jeseduardopi@gmail.com>
2021-03-10 11:10:44 +0100

    Classe

---
+++ src/Entity/Classe.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClasseRepository::class)
+ */
+class Classe
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $nom_classe;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomClasse(): ?string
+    {
+        return $this->nom_classe;
+    }
+
+    public function setNomClasse(string $nom_classe): self
+    {
+        $this->nom_classe = $nom_classe;
+
+        return $this;
+    }
+}
