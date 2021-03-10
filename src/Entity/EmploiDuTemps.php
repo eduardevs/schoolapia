73f96a2f1dde02709c9a5a8dd72f54dd338e0ece
Raphaël <raphael_froger@orange.fr>
2021-03-10 11:06:24 +0100

    Table Emploi Du Temps

---
+++ src/Entity/EmploiDuTemps.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmploiDuTempsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmploiDuTempsRepository::class)
+ */
+class EmploiDuTemps
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $heureDebut;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $heureFin;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $jour;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->heureDebut;
+    }
+
+    public function setHeureDebut(\DateTimeInterface $heureDebut): self
+    {
+        $this->heureDebut = $heureDebut;
+
+        return $this;
+    }
+
+    public function getHeureFin(): ?\DateTimeInterface
+    {
+        return $this->heureFin;
+    }
+
+    public function setHeureFin(\DateTimeInterface $heureFin): self
+    {
+        $this->heureFin = $heureFin;
+
+        return $this;
+    }
+
+    public function getJour(): ?\DateTimeInterface
+    {
+        return $this->jour;
+    }
+
+    public function setJour(\DateTimeInterface $jour): self
+    {
+        $this->jour = $jour;
+
+        return $this;
+    }
+}
